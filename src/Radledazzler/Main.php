c9fa373ec1291065409e6fff032520cf75addc60
Radledazzler <66074102+Radledazzler@users.noreply.github.com>
2020-08-03 12:55:21 -0300

    Update Main.php

--- src/Radledazzler/Main.php
+++ src/Radledazzler/Main.php
@@ -1,3 +1,23 @@
 <?php
 
+namespace: Radledazzler;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+
+class main extends PluginBase implements Listener {
+
+public function onEnable(){
+    $this->getLogger()->info("enabled")
+}
+
+public function onDisable(){
+    $this->getLogger()->info("disabled")
+}
+
+
 
